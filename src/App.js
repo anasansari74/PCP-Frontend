d547aa82bb168d5be84f941801d0cbe521daec5c
Duncan Magill <duncanmagill@hotmail.co.uk>
2022-01-15 14:59:55 +0000

    🏗: Creating fetch from external API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+	useEffect(() => {}, []);
 	return (
 		<div className="App">
 			<header className="App-header">
-                <h1>Positive Thought Generator</h1>
-            </header>
+				<h1>Positive Thought Generator</h1>
+			</header>
 			<main>
 				<section className="thought-box">
-					<p>You are amazing. What goes by you isn't for you. YOLO</p>
+					<p className="quote">
+						"You are amazing. What goes by you isn't for you. YOLO"
+					</p>
 				</section>
-				<button>Generate Thought</button>
+				<button
+					onClick={() => {
+						fetch("https://zenquotes.io/api/quotes")
+							.then((res) => {res => res.json()})
+                            .then(console.log())
+					}}
+				>
+					Generate Thought
+				</button>
 			</main>
 		</div>
 	);
