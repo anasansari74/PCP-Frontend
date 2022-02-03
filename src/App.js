80c8f8eaf4dbbfe4ac769af0e62542211610268f
brokensythe <ade_1.0@live.co.uk>
2022-02-03 12:32:00 +0000

    added options for thoughts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { useState } from "react";
 
 function App() {
 
-	
-
     const colors = ["#E9C46A", "#F4A261","#E76F51", "#F5AA6F", "#8CC084", "#2F4550", "#828E82" ]
 
     const randomColorString = colors[Math.floor(Math.random()* colors.length)]
@@ -17,23 +15,37 @@ function App() {
 				<h1>Positive Thought Generator</h1>
 			</header>
 			<main className="page-body">
-				<section className="thought-box">
-					<p className="quote">
+				<section>
+					<p className="thought-box">
 						{quote}
 					</p>
+					<select>
+						<option value="" disabled>Select the category you feel fits your thought best</option>
+						<option value="Health">Health</option>
+						<option value="Uplifting">Uplifting</option>
+						<option value="Career">Career</option>
+						<option value="Motivation">Motivation</option>
+						<option value="Relationships">Relationships</option>
+						<option value="Wholesome">Wholesome</option>
+						<option value="Relaxing">Relaxing</option>
+						<option value="Comedic">Comedic</option>
+						<option value="Happiness">Happiness</option>
+						<option value="Insightful">Insightful</option>
+						<option value="Random">Random</option>
+					</select>
+					<button
+						onClick={(e) => {
+							fetch("http://localhost:4000/quotes")
+								.then(res=>res.json())
+								.then(data=>{
+									setQuote(data[Math.floor(Math.random() * data.length)])
+									document.querySelector(".page-body").style.backgroundColor = randomColorString
+								})
+						}}
+					>
+						Generate Thought
+					</button>
 				</section>
-				<button
-					onClick={(e) => {
-						fetch("http://localhost:4000/quotes")
-							.then(res=>res.json())
-                            .then(data=>{
-								setQuote(data[Math.floor(Math.random() * data.length)])
-								document.querySelector(".page-body").style.backgroundColor = randomColorString
-							})
-					}}
-				>
-					Generate Thought
-				</button>
 			
                 <section className="post-thought">
                     <form >
