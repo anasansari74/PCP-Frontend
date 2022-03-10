717de537057d31e2950ab4bbe79fa7f502ae9a76
Anas Ansari <anas_ansar1@outlook.com>
2022-03-10 16:45:05 +0000

    Build: Styling and logic for first two sections

---
+++ src/lists.js
@@ -0,0 +1,22 @@
+export const colors = [
+  "#E9C46A",
+  "#F4A261",
+  "#E76F51",
+  "#F5AA6F",
+  "#8CC084",
+  "#828E82",
+];
+
+export const categories = [
+  "Random",
+  "Health",
+  "Uplifting",
+  "Career",
+  "Motivation",
+  "Relationships",
+  "Wholesome",
+  "Relaxing",
+  "Comedic",
+  "Happiness",
+  "Insightful",
+];
